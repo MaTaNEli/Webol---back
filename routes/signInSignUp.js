f55b09b2ac09dad5186daa4197872c90addfb79c
Matan <mataneliyahu@gmail.com>
2021-12-12 11:03:44 +0200

    temp change fixing all signup way

--- routes/signInSignUp.js
+++ routes/signInSignUp.js
@@ -2,21 +2,10 @@ const express = require('express');
 const passport = require ('passport');
 const jwt = require ('jsonwebtoken');
 const controller = require('../controller/signInSignUp');
-const auth = require ('./authenticated')
 
 const router = express.Router();
 
 router.post('/register', controller.registerPosts);
 
-router.post('/login', passport.authenticate('local', {
-    failureRedirect: '/blabla',
-    failureFlash: true
-    }),controller.logInPost);
-
-router.get('/blabla', (req, res) =>{
-    res.status(200).json('this is the end')
-});
-
-router.get('/imin', controller.logInPost);
 
 module.exports = router;
