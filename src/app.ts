f2f27ef05b6f58017bf20aaa103f3c312e6cb1dc
Matan <Mataneliyahu@gmail.com>
2022-05-08 18:23:42 +0300

    initialize the app

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import app from './server'
 
 // Server listen on http//localhost:3000
 (async () => {
-    console.log(1)
     await initStorage();
     app.listen(process.env.PORT || 3000, () => console.log('Server is running'));
 })();
